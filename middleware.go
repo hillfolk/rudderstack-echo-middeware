4c4a9c7689c2fa48ef3d632f2676fb6bcd36c810
JunYong <pjy1418@gmail.com>
2023-02-02 08:08:03 +0900

    feat: 예제 추가 & 오타 수정

--- middleware.go
+++ middleware.go
@@ -10,14 +10,12 @@ type ContextRudderStackType string // Path: middleware.go
 
 var ContextRudderStackName = "RUDDERSTACK" // Path: middleware.go
 
-func ContextRudderSatck(key, urls string) echo.MiddlewareFunc {
-	client := NewClient(key, urls)
+func ContextRudderSatck(client *Client) echo.MiddlewareFunc {
+
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			req := c.Request()
 			ctx := req.Context()
-
-			client := client.GetClient()
 			c.SetRequest(req.WithContext(context.WithValue(ctx, ContextClientName, client)))
 			return next(c)
 		}
